1f5728614df3f184bc4244b6347d0c00f6b19991
Nick Luckey <nickluckey@gmail.com>
2022-12-11 16:20:56 +0000

    Update devcontainer

--- .devcontainer/scripts/poststart.sh
+++ .devcontainer/scripts/poststart.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+
+DEV_C_ROOT="/workspaces/devcontainer/.devcontainer"
 RED='\033[0;31m'
 LBLUE='\033[0;34m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+
+init_setup() {
+  cp -r ${DEV_C_ROOT}/configs/. $HOME
+  envsubst < /workspaces/devcontainer/.devcontainer/configs/.aws/credentials > $HOME/.aws/credentials
+  
+  pass init -p aws-vault 202AFBD04D86E910
+  sudo chmod 600 ~/.ssh/id_ed*
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_ed25519
+
+  git config --global push.autoSetupRemote true
+}
 
 verify_tools() {
   tools=("aws" "aws-vault" "chamber" "docker" "go" "gpg" "java" "kubectl" "kubectl krew" "git-mob" "pre-commit" "python3" "ruby")
@@ -25,5 +39,6 @@ setup_kube(){
   aws-vault exec ldx.pipeline --no-session -- /workspaces/lighthouse-di-documentation/maintainers/scripts/onboarding/kubernetes-setup.sh
 }
 
+init_setup
 verify_tools
 setup_kube
